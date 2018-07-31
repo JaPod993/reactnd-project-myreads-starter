01359f8a9dfeeefb2c0990a4c4419e39b911fd26
Jan Podgorski <j.pod.12@gmail.com>
2018-07-31 20:21:08 +0200

    tring to fetch books form booksAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route } from "react-router-dom"
 import Books from "./books"
 import Search from "./search"
 
 class BooksApp extends Component {
+
+  state = {
+    books: []
+  };
+
+  componetDidMount() {
+    this.getBooks();
+  }
+
+  getBooks() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books});
+      console.log('My books are', this.state.books)
+    });
+  }
+
   render () {
     return (
         <div>
